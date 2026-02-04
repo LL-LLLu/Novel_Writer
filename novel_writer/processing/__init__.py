f882926b54223018caf47453622f3c5834d57e9a
Qi Lu <qilu@Qis-MacBook-Pro.local>
2026-02-03 20:50:16 -0600

    feat: add MinHash-based deduplication

--- novel_writer/processing/__init__.py
+++ novel_writer/processing/__init__.py
@@ -1,5 +1,11 @@
 from .clean import clean_data
 from .format import format_data
 from .segment import segment_directory
+from .deduplicate import deduplicate_dataset
 
-__all__ = ["clean_data", "format_data", "segment_directory"]
+__all__ = [
+    "clean_data",
+    "format_data",
+    "segment_directory",
+    "deduplicate_dataset"
+]
