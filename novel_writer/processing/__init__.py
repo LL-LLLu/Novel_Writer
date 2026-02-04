f522083206c3161bcc9cc10f3611ab47da1ef054
Qi Lu <qilu@Qis-MacBook-Pro.local>
2026-02-03 20:42:51 -0600

    refactor: create package structure for CLI refactoring

---
+++ novel_writer/processing/__init__.py
@@ -0,0 +1,4 @@
+from .clean import clean_data
+from .format import format_data
+
+__all__ = ["clean_data", "format_data"]
---
+++ novel_writer/utils/__init__.py
@@ -0,0 +1,4 @@
+from .logger import setup_logger
+from .progress import create_progress
+
+__all__ = ["setup_logger", "create_progress"]
