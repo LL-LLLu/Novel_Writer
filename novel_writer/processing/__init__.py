4893e32a66ffdcd40deaa82f3287ba008a17ae9d
Qi Lu <qilu@Qis-MacBook-Pro.local>
2026-02-03 20:49:40 -0600

    feat: add intelligent chapter segmentation

--- novel_writer/processing/__init__.py
+++ novel_writer/processing/__init__.py
@@ -1,4 +1,5 @@
 from .clean import clean_data
 from .format import format_data
+from .segment import segment_directory
 
-__all__ = ["clean_data", "format_data"]
+__all__ = ["clean_data", "format_data", "segment_directory"]
