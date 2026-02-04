6dbef75ed687a92bfc2ee525905a44b4d2f47ac3
Qi Lu <qilu@Qis-MacBook-Pro.local>
2026-02-03 21:29:33 -0600

    feat: add instruction pair generation

--- novel_writer/processing/__init__.py
+++ novel_writer/processing/__init__.py
@@ -3,11 +3,13 @@ from .format import format_data
 from .segment import segment_directory
 from .deduplicate import deduplicate_dataset
 from .filter import filter_dataset
+from .instruct import generate_instruct_dataset
 
 __all__ = [
     "clean_data",
     "format_data",
     "segment_directory",
     "deduplicate_dataset",
-    "filter_dataset"
+    "filter_dataset",
+    "generate_instruct_dataset"
 ]
